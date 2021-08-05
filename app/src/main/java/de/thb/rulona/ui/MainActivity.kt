3a5b3c1b7446069610cb8786d468ebe3260338cb
andrethiele <thielea@th-brandenburg.de>
2021-08-05 11:45:18 +0200

    begin dialog impl

--- app/src/main/java/de/thb/rulona/ui/MainActivity.kt
+++ app/src/main/java/de/thb/rulona/ui/MainActivity.kt
@@ -3,9 +3,30 @@ package de.thb.rulona.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
 import com.google.accompanist.insets.ProvideWindowInsets
 import de.thb.ui.theme.RulonaTheme
+import de.thb.ui.theme.margin_large
+import de.thb.ui.theme.margin_medium
+import de.thb.ui.util.state
 
 class MainActivity : ComponentActivity() {
 
@@ -14,8 +35,61 @@ class MainActivity : ComponentActivity() {
         setDecorFitsSystemWindows(window, false)
 
         setContent {
-            ProvideWindowInsets {
-                RulonaTheme {
+            RulonaTheme {
+                var dialogVisible by state { true }
+
+                LaunchedEffect(dialogVisible) {
+                    if (dialogVisible) {
+                        // TODO save onboarding has been seen
+                    }
+                }
+
+                if (dialogVisible) {
+                    Dialog(
+                        onDismissRequest = { dialogVisible = false },
+                        content = {
+                            Card(
+                                shape = RoundedCornerShape(12.dp),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ) {
+                                Column(
+                                    modifier = Modifier.padding(margin_large),
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text(
+                                        text = "Alles auf einen Blick",
+                                        style = MaterialTheme.typography.h5
+                                    )
+
+                                    Spacer(modifier = Modifier.height(margin_medium))
+
+                                    Text(
+                                        style = MaterialTheme.typography.body1,
+                                        text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod"
+                                    )
+
+                                    Spacer(modifier = Modifier.height(margin_large.times(2)))
+
+                                    Button(onClick = {}) {
+                                        Text(text = "Weiter")
+                                    }
+
+                                    Spacer(modifier = Modifier.height(margin_medium))
+
+                                    TextButton(onClick = { dialogVisible = false }) {
+                                        Text(
+                                            text = "Überspringen",
+                                            style = MaterialTheme.typography.body1
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    )
+                }
+
+                ProvideWindowInsets {
                     AppContainer()
                 }
             }
